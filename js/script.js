0ca9fed7dc01551cee49d91bd13580b918af2672
Ulya Nafisatussaniyah <ulyanafisa0@gmail.com>
2023-05-19 13:45:52 +0700

    skills

--- js/script.js
+++ js/script.js
@@ -41,15 +41,14 @@ window.addEventListener("scroll", function() {
     });
 
     /*=====Sticky navbar=====*/
-    window.addEventListener('scroll', function() {
+     window.addEventListener('scroll', function() {
       var navbar = document.getElementById('navbar');
-      if (window.scrollY > 0) {
+       if (window.scrollY > 0) {
         navbar.classList.add('sticky');
       } else {
         navbar.classList.remove('sticky');
       }
     });
-    
 
     /*=====remove toggle icon=====*/
     menuIcon.classList.remove('bx-x');
